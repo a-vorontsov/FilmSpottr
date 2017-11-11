8b9644cdb9e5b0ed9e6aeee320c623e50a4666db
James Cowley <james_cowley@live.co.uk>
2017-11-11 20:30:28 +0000

    first 2000 movies processed + geocoding setup

--- scraping/scrapeIMDb.py
+++ scraping/scrapeIMDb.py
@@ -6,14 +6,14 @@ print "Imports done."
 
 # function to scrape locations from imdb
 def scrapeLocations(locationsPageURL):
-	print "\n > Looking at URL "+url
+	#print "\n > Looking at URL "+url
 	pagemovieData = urllib2.urlopen(url)
 	imdbID = re.sub("http://www.imdb.com/title/","",locationsPageURL)
 	imdbID = re.sub("/locations","",imdbID)
 	movieData = []
 	movieData.append(imdbID)
-#	locationInfo = ["",""]
 	address = ""
+	
 	# read imdb page line by line
 	for pageline in pagemovieData:
 
@@ -29,9 +29,7 @@ def scrapeLocations(locationsPageURL):
 				continue
 			elif "itemprop='url'>" in pageline: # address
 				pageline = re.sub("itemprop='url'>","",pageline)[:-1]
-				#movieData.append(pageline.strip())
 				address = str(pageline.strip())
-#				locationInfo[0] = str(pageline.strip())
 				#print " > > location: "+pageline
 		
 		# location description
@@ -39,14 +37,8 @@ def scrapeLocations(locationsPageURL):
 				pageline = re.sub("</dd>","",pageline)
 				pageline = re.sub("\(","",pageline)
 				pageline = re.sub("\)","",pageline)[:-1]
-				#movieData.append(pageline.strip())
-				#description = pageline.strip()
-#				locationInfo[1] = str(pageline.strip())
-#				locationInfoTemp = locationInfo
-				#movieData.append(locationInfoTemp)
 				movieData.append([address,pageline.strip()])
-#				print locationInfo
-#				print " > > > description: "+pageline
+				#print " > > > description: "+pageline
 		elif "<h2>See also</h2>" in pageline: # locations are over
 			break
 		elif "It looks like we don't have any Filming Locations for this title yet." in pageline:
@@ -54,24 +46,42 @@ def scrapeLocations(locationsPageURL):
 			break
 	return movieData
 
-# read list of IMDb urls
-urlFileName='imdbURLsONLY.dat'
-urlFile = open(urlFileName,'r')
-for line in urlFile:
-	url = line.strip()+"locations"
-	movieData = scrapeLocations(url)
+# output file
+outputFile = open('imdbAddresses.csv','w') # 'a' means append, so you won't overwrite anything in the file already
+outputFile.write("----------")
 
-	# construct output
-	outputStub = movieData[0]+","+movieData[1]+","
-	if movieData[2] is "NOLOCATIONS":
-		print " > No location information."
+# read list of IMDb urls
+urlFileName='imdbURLsTIDY.dat'
+urlFile = open(urlFileName,'r')
+nLines = sum(1 for line in open(urlFileName))
+print " > Number of lines = "+str(nLines)
+startPoint = 1012 # number of lines to skip
+currentLine = 0
+for line in urlFile:
+	try:
+		currentLine = currentLine +1
+		if currentLine < startPoint:
+			continue
+		print " > Processing URL "+str(currentLine)+"/"+str(nLines)+" = "+str(round(float(currentLine)/float(nLines),2))+"%"
+		url = line.strip()+"locations"
+		movieData = scrapeLocations(url)
+		
+		# construct output
+		outputStub = movieData[0]+","+movieData[1]+","
+		if movieData[2] is "NOLOCATIONS":
+			print " > No location information."
+			continue
+		else:
+			for datum in movieData[2:]:
+				output = movieData[0]+","+movieData[1]+",\""+datum[0]+"\","+datum[1]+",,\n"
+				outputFile.write(output)
+	except Exception, e:
+		print " > Got an error: "+str(e)+" from URL: "+str(line)
+		print " > Moving on..."
 		continue
-	else:
-		for datum in movieData:#[2:]:
-#			None
-			print datum
-#		for i, datum in enumerate(movieData[2:]):
-#			None
+
+outputFile.close()
+print " > Done!"
 
 
 
