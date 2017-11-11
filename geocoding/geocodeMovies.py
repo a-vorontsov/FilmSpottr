4326c8bf00ccd954449779155042c42027e1a9ed
James Cowley <james_cowley@live.co.uk>
2017-11-11 22:23:52 +0000

    complete geocoding of 1753 UK film locations + descriptions

--- geocoding/geocodeMovies.py
+++ geocoding/geocodeMovies.py
@@ -2,13 +2,28 @@
 # original code by Peter
 
 print "Importing libraries..."
-import googlemaps, csv
+import googlemaps, csv, time
 print "Imports done."
 
-def addGeocode(address):
+def getGeocode(address):
 #	gmaps = googlemaps.Client(key = 'AIzaSyBOrSOIY_pfVSx-R9le50DShnzzIOqFAT8') # peter's used up API key
 	gmaps = googlemaps.Client(key = 'AIzaSyCZVQx1F60aPbLcJUn5JUprEC_3Au2Gi-k') # james' fresh API key
-	gmapsReply = googlemaps.geocoding.geocode(gmaps,address = address)
+	gmapsReply = 0
+	nFails = 0
+	while True:
+		try:
+			gmapsReply = googlemaps.geocoding.geocode(gmaps,address = address)
+			break
+		except Exception, e:
+			print " > Got an error: "+str(e)
+			print " > Waiting 10 seconds and trying again..."
+			nFails = nFails +1
+			time.sleep(10)
+			if nFails > 10:
+				print " > failed too much, giving up."
+				return -1,-1
+			continue
+
 	if len(gmapsReply) < 1:
 		print " > API fail, moving on..."
 		return -1,-1
@@ -22,7 +37,7 @@ def getAddress(reader,writer):
 #		with open(outputFileName, 'wb') as csvOutFile:
 	for row in reader:
 		if row[2] != "sceneAddress":
-			lng, lat = addGeocode(row[2])
+			lng, lat = getGeocode(row[2])
 			if lng == -1 and lat == -1:
 				continue
 			row = row[:-2] # cut last 2 fields
@@ -34,8 +49,9 @@ def getAddress(reader,writer):
 
 #inName = "imdbAddresses0-1014.csv"
 #inName = "addresses_3_UKONLYWITHDESCRIPTIONS.csv"
-inName = "ukFilmLocations_NOGEOCODES.csv"
-outName = "ukFilmLocations_WITH_GEOCODES.csv"
+#inName = "ukFilmLocations_NOGEOCODES.csv"
+inName = "bulkAddressesUKonlyDescribed.csv"
+outName = "bulkAddressesUKonlyDescribed_GEOCODED.csv"
 csvInFile = open(inName, 'r')#rb
 csvOutFile = open(outName, 'w')#wb
 reader = csv.reader(csvInFile, delimiter = ',', quotechar = '"')
@@ -52,7 +68,7 @@ for row in reader:
 	if row[2] == "sceneAddress":
 		writer.writerow(row)
 	else:
-		lng, lat = addGeocode(row[2])
+		lng, lat = getGeocode(row[2])
 		if lng == -1 and lat == -1:
 			continue
 		row = row[:-2] # cut last 2 fields
